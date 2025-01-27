3237954fa08994927fe9ce088650e4ab98b251c6
Gabriel Cavalier <gacavali@c4r2p1.42nice.fr>
2025-01-27 14:17:22 +0100

    save

--- Module09/ex00/ BitcoinExchange.hpp
+++ Module09/ex00/ BitcoinExchange.hpp
@@ -6,11 +6,17 @@
 /*   By: gacavali <gacavali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 09:55:36 by gacavali          #+#    #+#             */
-/*   Updated: 2025/01/27 09:56:03 by gacavali         ###   ########.fr       */
+/*   Updated: 2025/01/27 14:16:05 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
 #include <string>
 #include <iostream>
 #include <map>
 
+class BitcoinExchange.hpp
+
+#endif
--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -6,13 +6,19 @@
 /*   By: gacavali <gacavali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 09:55:04 by gacavali          #+#    #+#             */
-/*   Updated: 2025/01/27 10:53:13 by gacavali         ###   ########.fr       */
+/*   Updated: 2025/01/27 14:15:44 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+
 int	main(int argc, char **argv)
 {
-	
+	if (argc == 2)
+	{
+		
+	}
+	else
+		std::cout << "\033[31mError : Need one argument\033[0m"
 }
